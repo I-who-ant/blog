96bf4f36a0686cfda479f5ed977acaaef10ff42d
I-who-ant <2391934808@qq.com>
2025-10-04 12:02:05 +0800

    chore: tune markdown highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,22 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://example.com',
+  markdown: {
+    syntaxHighlight: {
+      type: 'shiki',
+      excludeLangs: ['math'],
+    },
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'one-dark-pro',
+      },
+      defaultColor: false,
+      wrap: true,
+      langAlias: {
+        cjs: 'javascript',
+        tsx: 'typescript',
+      },
+    },
+  },
 });
