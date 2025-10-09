c6f0c6251e33b9a2bfe5f758c45a357cd6987cee
I-who-ant <2391934808@qq.com>
2025-10-09 10:08:42 +0800

    forming

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
+import markdoc from '@astrojs/markdoc';
+
 export default defineConfig({
   site: 'https://seeback.xyz',
+
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
@@ -17,4 +22,6 @@ export default defineConfig({
       },
     },
   },
+
+  integrations: [mdx(), markdoc()],
 });
